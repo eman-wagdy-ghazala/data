abca1d2af0f07d0929302523b98d975ea7a4dcc1
eman-wagdy-ghazala <126484659+eman-wagdy-ghazala@users.noreply.github.com>
2023-03-06 20:58:11 +0200

    Add files via upload

---
+++ دكريم.sql
@@ -0,0 +1,21 @@
+create database if not exists ahaijka ;
+create table if not exists customers(
+customers_id int primary key,
+customers_name varchar(225) not null,
+phone varchar(225) not null
+);
+
+create table if not exists orders(
+order_id int primary key ,
+order_data  date not null,
+constraint_id int,
+ constraint orderby foreign key(customar_id) references customers(customer_id)
+ );
+ 
+ create table if not exists vary(
+ vary_id int primary key,
+ renuction varchar (225) not null default'food',
+ order_id int,
+ foreign key(order_id) references orders(order_id)
+ );
+ 
